7bfc98762e0ec69ef4993173470758847a2f4227
MrHouston <mrhoustonvr@yandex.ru>
2026-06-04 18:11:19 +0300

    Инициализация проекта: Vite + React + Дизайн-система (переменные, шрифты, структура)

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1 style={{ color: 'var(--accent)' }}>Мяу! 🐾</h1>
+    </div>
+  )
+}
+
+export default App
